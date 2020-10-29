161e32518406890ca9deb93902ab465269ea7fa3
mamoru18 <mamoru.081024@gmail.com>
2020-10-29 19:03:28 +0900

    朝活への参加処理完了

--- app/Http/Controllers/Api/LineController.php
+++ app/Http/Controllers/Api/LineController.php
@@ -40,21 +40,33 @@ class LineController extends Controller
 			foreach ($events as $event) {
 				// 入力した文字取得
 				$message = $event->getText();
-				if (strcmp($message, '参加します') == 0) {
+				if (strcmp($message, '朝活に参加します') == 0) {
+					//メッセージを送信したユーザーのIDを取得
 					$user_id = $event->getUserId();
+					//ユーザー情報を取得するメソッドを使用
 					$response = $lineBot->getProfile($user_id);
+					//取得した情報をJSONデコードする
 					$profile = $response->getJSONDecodedBody();
 					$user_display_name = $profile['displayName'];
-					$user = new User();
-					$input = ['name' => $user_display_name, 'user_identifier' => $user_id, 'oversleeping_times' => 0];
-					$user->fill($input)->save();
-					$replyToken = $event->getReplyToken();
-					$text = '朝活頑張りましょう！';
-					$textMessage = new TextMessageBuilder($text);
-//					$lineBot->replyMessage($replyToken, $textMessage);
+//					error_log(print_r($user_id, true) . '\n', 3, '/var/www/html/log.txt');
+					$user_exist = User::where('user_identifier', $user_id)->exists();
+					error_log(print_r($user_exist, true) . '\n', 3, '/var/www/html/log.txt');
+					//ユーザーが登録済みかどうか判断する
+					if ($user_exist) {
+						$replyToken = $event->getReplyToken();
+						$message = $user_display_name . 'さんは参加登録済みです！朝活頑張りましょう！';
+					} else {
+						$user = new User();
+						$input = ['name' => $user_display_name, 'user_identifier' => $user_id, 'oversleeping_times' => 0];
+						$user->fill($input)->save();
+						$replyToken = $event->getReplyToken();
+						$message = $user_display_name . 'さんようこそ！朝活頑張りましょう！';
+					}
+					$textMessage = new TextMessageBuilder($message);
+					$lineBot->replyMessage($replyToken, $textMessage);
 				}
-				$replyToken = $event->getReplyToken();
-				$textMessage = new TextMessageBuilder($message);
+//				$replyToken = $event->getReplyToken();
+//				$textMessage = new TextMessageBuilder($message);
 //				$lineBot->replyMessage($replyToken, $textMessage);
 			}
 		} catch (Exception $e) {
