c19b8f37e753b42d1c3a9739faf10381fb7fc4e5
mamoru18 <mamoru.081024@gmail.com>
2020-11-08 15:47:51 +0900

    messageイベントのif文を追加、寝坊回数お知らせ機能追加、重複部分を削除

--- app/Http/Controllers/Api/LineController.php
+++ app/Http/Controllers/Api/LineController.php
@@ -12,7 +12,9 @@ use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use Exception;
 use App\User;
 use App\Message;
-//use Illuminate\Support\Carbon;
+//デバッグ用
+//$result = 'ここまできてるよ';
+//error_log(print_r($result, true) . "\n", 3, '/var/www/html/log.txt');
 
 class LineController extends Controller
 {
@@ -20,8 +22,6 @@ class LineController extends Controller
 	{
 		$lineAccessToken = config('line.line_access_token', "");
 		$lineChannelSecret = config('line.line_channel_secret', "");
-//		error_log(print_r($lineAccessToken, true) . '\n', 3, '/var/www/html/log.txt');
-//		error_log(print_r($lineChannelSecret, true) . '\n', 3, '/var/www/html/log.txt');
 
 		// 署名のチェック
 		$signature = $request->headers->get(HTTPHeader::LINE_SIGNATURE);
@@ -39,91 +39,73 @@ class LineController extends Controller
 			// イベント取得
 			$events = $lineBot->parseEventRequest($request->getContent(), $signature);
 			foreach ($events as $event) {
-				// 入力した文字取得
-				$line_message = $event->getText();
-				//メッセージを送信したユーザーのIDを取得
-				$user_id = $event->getUserId();
-//				error_log(print_r($user_id, true) . '\n', 3, '/var/www/html/log.txt');
-				//ユーザーの存在を確認
-				$user_exist = User::where('user_identifier', $user_id)->exists();
-//				error_log(print_r($user_exist, true) . '\n', 3, '/var/www/html/log.txt');
-				//朝活への参加処理
-				if (strcmp($line_message, '朝活に参加します') == 0) {
-					//ユーザー情報を取得するメソッドを使用
-					$response = $lineBot->getProfile($user_id);
-					//取得した情報をJSONデコードする
-					$profile = $response->getJSONDecodedBody();
-//					error_log(print_r($profile, true) . '\n', 3, '/var/www/html/log.txt');
-					$user_display_name = $profile['displayName'];
-					//ユーザーが登録済みかどうか判断する
-					if ($user_exist) {
-						//ユーザーが登録済みの場合
+				$event_type = $event->getType();
+				if ($event_type === 'message') {
+					// 入力した文字取得
+					$line_message = $event->getText();
+					//メッセージを送信したユーザーのIDを取得
+					$user_identifier = $event->getUserId();
+					//ユーザーの存在を確認
+					$user_exist = User::where('user_identifier', $user_identifier)->exists();
+					//朝活への参加処理
+					if (strpos($line_message, '朝活') !== false && strpos($line_message, '参加') !== false) {
+						//ユーザー情報を取得するメソッドを使用
+						$response = $lineBot->getProfile($user_identifier);
+						//取得した情報をJSONデコードする
+						$profile = $response->getJSONDecodedBody();
+						$user_display_name = $profile['displayName'];
+						//ユーザーが登録済みかどうか判断する
+						if ($user_exist) {
+							//ユーザーが登録済みの場合
+							$text_message = $user_display_name . 'さんは参加登録済みです！朝活頑張りましょう！';
+						} else {
+							//ユーザーが未登録の場合
+							$user = new User();
+							$input = ['name' => $user_display_name, 'user_identifier' => $user_identifier];
+							$result = $user->fill($input)->save();
+							$text_message = $user_display_name . 'さんようこそ！朝活頑張りましょう！';
+						}
 						$replyToken = $event->getReplyToken();
-						$text_message = $user_display_name . 'さんは参加登録済みです！朝活頑張りましょう！';
-					} else {
-						//ユーザーが未登録の場合
-						$user = new User();
-						$input = ['name' => $user_display_name, 'user_identifier' => $user_id, 'oversleeping_times' => 0];
-						$result = $user->fill($input)->save();
-//						error_log(print_r($result, true) . "\n", 3, '/var/www/html/log.txt');
-						$replyToken = $event->getReplyToken();
-						$text_message = $user_display_name . 'さんようこそ！朝活頑張りましょう！';
+						$textMessage = new TextMessageBuilder($text_message);
+						$lineBot->replyMessage($replyToken, $textMessage);
 					}
-					$textMessage = new TextMessageBuilder($text_message);
-					$lineBot->replyMessage($replyToken, $textMessage);
-				}
-				//メッセージ保存処理
-				//明日はパスする処理
-				if ($user_exist) {
-					$user = User::where('user_identifier', $user_id)->first();
-					$user_id = $user->id;
-					// } の//も消す
-					if (strpos($line_message, '明日') !== false || strpos($line_message, 'パス') !== false || strpos($line_message, '起き') !== false) {
-						if (strtotime(date('H:i:s')) < strtotime('6:00:00') || strtotime('22:00:00') <= strtotime(date('H:i:s'))) {
-							$result = 'その中にはいってきた';
-							error_log(print_r($result, true) . "\n", 3, '/var/www/html/log.txt');
-//							$user = User::where('user_identifier', $user_id)->first();
-//							$user_id = $user->id;
-//							$user->oversleep_check = 1;
-//							$user->save();
-							//上で明日早起きしない処理を完了しているのて、受信メッセージのDBへの保存は必要ないかも
-//							try {
+					//メッセージ保存処理
+					if ($user_exist) {
+						$user = User::where('user_identifier', $user_identifier)->first();
+						$user_id = $user->id;
+						//明日はパスする処理
+						if (strpos($line_message, '明日') !== false || strpos($line_message, 'パス') !== false || strpos($line_message, '起き') !== false) {
+							if (strtotime(date('H:i:s')) < strtotime('6:00:00') || strtotime('22:00:00') <= strtotime(date('H:i:s'))) {
+								$message = new Message();
+								$input = ['user_id' => $user_id, 'message' => $line_message];
+								$message->fill($input)->save();
+								$text_message = '承知しました。ぐっすり寝てくださいな。';
+							//以下は必要に応じて使う
+	/*						} else {
+								$text_message = '今寝ればまだ間に合うよ！！！';*/
+							}
+						}
+						//DBへメッセージを保存する時間帯を指定
+						if (strtotime('6:00:00') <= strtotime(date('H:i:s')) && strtotime(date('H:i:s')) < strtotime('7:00:00')) {
 							$message = new Message();
 							$input = ['user_id' => $user_id, 'message' => $line_message];
 							$message->fill($input)->save();
-							error_log(print_r($message, true) . "\n", 3, '/var/www/html/log.txt');
-							$text = '承知しました。ぐっすり寝てくださいな。';
-/*							} catch (\Exception $e) {
-								error_log(print_r($e, true) . "\n", 3, '/var/www/html/log.txt');
+							//以下は必要に応じて使う
+//							$text_message = 'おはようございます！';
+						}
+						//現在の寝坊回数をお知らせする
+						if (strpos($line_message, '寝坊') !== false && strpos($line_message, '回数') !== false) {
+							$text_message = "現在の寝坊回数\n";
+							$users = User::all();
+							foreach($users as $user) {
+								$text_message .= $user->name . 'さんは「' . $user->oversleeping_times . "回」です！\n";
 							}
-							exit;*/
-							$textMessage = new TextMessageBuilder($text);
-							error_log(print_r($text, true) . "\n", 3, '/var/www/html/log.txt');
-						} else {
-							$text = '今寝ればまだ間に合うよ！！！';
-							$textMessage = new TextMessageBuilder($text);
-							error_log(print_r($text, true) . "\n", 3, '/var/www/html/log.txt');
+							$text_message .= "みなさん朝から頑張ってますね！\n今日も最高の１日にしましょう！！";
 						}
 						$replyToken = $event->getReplyToken();
+						$textMessage = new TextMessageBuilder($text_message);
 						$lineBot->replyMessage($replyToken, $textMessage);
-						error_log(print_r($text, true) . "\n", 3, '/var/www/html/log.txt');
 					}
-					//最終的に時間を12時から23時に変更する
-					//DBへメッセージを保存する時間帯を指定
-//					if (strtotime(date('H:i:s')) < strtotime('7:00:00') || strtotime('12:00:00') < strtotime(date('H:i:s'))) {
-					if (strtotime('6:00:00') <= strtotime(date('H:i:s')) && strtotime(date('H:i:s')) < strtotime('7:00:00')) {
-						$result = 'おはよう';
-						error_log(print_r($result, true) . "\n", 3, '/var/www/html/log.txt');
-//						$user = User::where('user_identifier', $user_id)->first();
-//						$user_id = $user->id;
-						$message = new Message();
-						$input = ['user_id' => $user_id, 'message' => $line_message];
-						$message->fill($input)->save();
-						$text = 'おはようございます！';
-						$textMessage = new TextMessageBuilder($text);
-					}
-					$replyToken = $event->getReplyToken();
-					$lineBot->replyMessage($replyToken, $textMessage);
 				}
 			}
 		} catch (Exception $e) {
