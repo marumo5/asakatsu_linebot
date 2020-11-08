922e243552a10c643dd27a27f1fed972aa38ba64
mamoru18 <mamoru.081024@gmail.com>
2020-11-08 16:03:17 +0900

    明日パスのif文条件を変更、「明日」を必ず含み、「パス」または「起き」を含む場合処理を実行する

--- app/Http/Controllers/Api/LineController.php
+++ app/Http/Controllers/Api/LineController.php
@@ -74,7 +74,7 @@ class LineController extends Controller
 						$user = User::where('user_identifier', $user_identifier)->first();
 						$user_id = $user->id;
 						//明日はパスする処理
-						if (strpos($line_message, '明日') !== false || strpos($line_message, 'パス') !== false || strpos($line_message, '起き') !== false) {
+						if (strpos($line_message, 'パス') !== false || strpos($line_message, '起き') !== false && strpos($line_message, '明日') !== false) {
 							if (strtotime(date('H:i:s')) < strtotime('6:00:00') || strtotime('22:00:00') <= strtotime(date('H:i:s'))) {
 								$message = new Message();
 								$input = ['user_id' => $user_id, 'message' => $line_message];
