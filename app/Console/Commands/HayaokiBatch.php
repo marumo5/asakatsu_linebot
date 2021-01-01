fbd6fb009d2d032eb0bb5868bb35608962794e6f
mamoru18 <mamoru.081024@gmail.com>
2021-01-01 18:08:36 +0900

    不要部分を削除

--- app/Console/Commands/HayaokiBatch.php
+++ app/Console/Commands/HayaokiBatch.php
@@ -6,11 +6,8 @@ use Illuminate\Console\Command;
 use App\User;
 use App\Message;
 use LINE\LINEBot;
-//use LINE\LINEBot\Constant\HTTPHeader;
-//use LINE\LINEBot\SignatureValidator;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-//use Exception;
 use Illuminate\Support\Facades\DB;
 
 class HayaokiBatch extends Command
@@ -51,7 +48,6 @@ class HayaokiBatch extends Command
 		foreach($affiliation_ids as $affiliation_id) {
 			DB::transaction(function() use($affiliation_id) {
 				$affiliation_id = $affiliation_id->affiliation_id;
-				error_log(print_r($affiliation_id, true) . "\n", 3, '/var/www/html/log.txt');
 				$text_message = null;
 				$messages = Message::where('affiliation_id', $affiliation_id)->get();
 				foreach($messages as $message) {
@@ -81,43 +77,9 @@ class HayaokiBatch extends Command
 				$textMessageBuilder = new TextMessageBuilder($text_message);
 				$response = $lineBot->pushMessage($affiliation_id, $textMessageBuilder);
 				$response->getHTTPStatus() . ' ' . $response->getRawBody();
-				echo $text_message;
-				error_log(print_r($text_message, true) . "\n", 3, '/var/www/html/log.txt');
 				User::where('oversleep_check', 1)->update(['oversleep_check' => 0]);
 			}, 5);
 		}
 		Message::truncate();
-		echo "成功です\n";
-    }
-		//プッシュメッセージの送信
-/*		$lineAccessToken = config('line.line_access_token', "");
-		$lineChannelSecret = config('line.line_channel_secret', "");
-		$httpClient = new CurlHTTPClient($lineAccessToken);
-		$lineBot = new LINEBot($httpClient, ['channelSecret' => $lineChannelSecret]);
-		if (!$text_message) {
-			$text_message = '寝坊なし！さすがです！！';
-		}
-		$textMessageBuilder = new TextMessageBuilder($text_message);
-		$response = $lineBot->pushMessage($user->user_identifier, $textMessageBuilder);
-//			echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-//			echo $text_message;
-		error_log(print_r($text_message, true) . "\n", 3, '/var/www/html/log.txt');
-	}*/
-//以下は必要であれば、書き直す。寝坊回数は聞かれたら答えるようにする.以下は個人ラインに送信する方法
-/*		$text_message = null;
-		$users = User::all();
-		foreach($users as $user) {
-			$text_message = $user->name . 'さんの現在の寝坊回数は' . $user->oversleeping_times . "回です！\n";
-			$text_message .= $user->name . "さん朝から頑張ってますね！\n今日も最高の１日にしましょう！！";
-			//プッシュメッセージの送信
-			$lineAccessToken = config('line.line_access_token', "");
-			$lineChannelSecret = config('line.line_channel_secret', "");
-			$httpClient = new CurlHTTPClient($lineAccessToken);
-			$lineBot = new LINEBot($httpClient, ['channelSecret' => $lineChannelSecret]);
-			$textMessageBuilder = new TextMessageBuilder($text_message);
-			$response = $lineBot->pushMessage($user->user_identifier, $textMessageBuilder);
-			echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-			echo $text_message;
-			error_log(print_r($text_message, true) . "\n", 3, '/var/www/html/log.txt');
-		}*/
+	}
 }
